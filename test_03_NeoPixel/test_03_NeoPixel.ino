17d6dadc63f4930a1efd84629f5c7b5d15464e69
Sophia L <Sophia L>
2016-10-19 03:26:28 -0400

    Begin new sketch for NeoPixel light up

---
+++ test_03_NeoPixel/test_03_NeoPixel.ino
@@ -0,0 +1,14 @@
+/*
+ * Test 2 : NeoPixel Ring
+ * Description : The following code lgihts up the NeoPixel Ring
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
