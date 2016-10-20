bdbb12af31b7a24faeef0bb925627b89fbc685fa
Sophia L <Sophia L>
2016-10-20 02:43:18 -0400

    Initializing Neopixel Ring in setup

--- test_04_pulseSensor/test_04_pulseSensor.ino
+++ test_04_pulseSensor/test_04_pulseSensor.ino
@@ -6,9 +6,22 @@
  * Edits: The BPM is printed out when a pulse is detected
  */
 
+//Adafruit NeoPixel ring library
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
 //  Variables
 int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
-int blinkPin = 13;                // pin to blink led at each beat
+int blinkPin = 13;                // Pin to blink led at each beat
+#define PIN 6                     // Pin to blink NeoPixel Ring
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+// NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
 
 // Volatile Variables, used in the interrupt service routine!
 volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
@@ -20,7 +33,11 @@ volatile boolean QS = false;        // becomes true when Arduino finds a beat.
 void setup(){
   pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
   Serial.begin(115200);             // we agree to talk fast!
-  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+  //Sets up to read Pulse Sensor signal every 2mS
+  interruptSetup();
+  //Sets up NeoPixel Ring
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop(){
