9b3b5ae59140fe1fb9499aab58dabae70e51ffcc
Sophia L <Sophia L>
2016-10-20 01:35:34 -0400

    BPM printed out properly. LED not blinking

--- test_04_pulseSensor/test_04_pulseSensor.ino
+++ test_04_pulseSensor/test_04_pulseSensor.ino
@@ -1,26 +1,14 @@
 /*
  * Test 4 : Pulse sensor
- * Description: The following code prints out the readings 
- * of the pulse sensor
+ * Description: The following code prints out the BPM readings 
+ * of the pulse sensor. An LED blinks to user's live heartbeat.
+ * Original Pulse Sensor Amped 1.4 code taken from Joel Murphy and Yury Gitman at http://www.pulsesensor.com
+ * Edits: The BPM is printed out when a pulse is detected
  */
-
- 
-/*  Pulse Sensor Amped 1.4    by Joel Murphy and Yury Gitman   http://www.pulsesensor.com
-----------------------  Notes ----------------------  ---------------------- 
-This code:
-1) Blinks an LED to User's Live Heartbeat   PIN 13
-2) Fades an LED to User's Live HeartBeat
-3) Determines BPM
-4) Prints All of the Above to Serial
-Read Me:
-https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/README.md   
- ----------------------       ----------------------  ----------------------
-*/
 
 //  Variables
 int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
 int blinkPin = 13;                // pin to blink led at each beat
-int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
 int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
 
 // Volatile Variables, used in the interrupt service routine!
@@ -30,47 +18,18 @@ volatile int IBI = 600;             // int that holds the time interval between 
 volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 
-// Regards Serial OutPut  -- Set This Up to your needs
-static boolean serialVisual = false;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
-
-
 void setup(){
   pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
-  pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
   Serial.begin(115200);             // we agree to talk fast!
-  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
-   // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE, 
-   // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
-//   analogReference(EXTERNAL);   
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
 }
 
-
-//  Where the Magic Happens
 void loop(){
-  
-    serialOutput() ;       
-    
-  if (QS == true){     // A Heartbeat Was Found
-                       // BPM and IBI have been Determined
-                       // Quantified Self "QS" true when arduino finds a heartbeat
-        fadeRate = 255;         // Makes the LED Fade Effect Happen
-                                // Set 'fadeRate' Variable to 255 to fade LED with pulse
-        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.     
-        QS = false;                      // reset the Quantified Self flag for next time    
-  }
-     
-  ledFadeToBeat();                      // Makes the LED Fade Effect Happen 
-  delay(20);                             //  take a break
+  if (QS == true){                       // Quantified Self flag is true when arduino finds a heartbeat
+    Serial.print("BPM = ");
+    Serial.println(BPM);
+  } 
 }
 
-
-
-
-
-void ledFadeToBeat(){
-    fadeRate -= 15;                         //  set LED fade value
-    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
-    analogWrite(fadePin,fadeRate);          //  fade LED
-  }
 
 
